b3320d6fbb9b482c5e38c53f78d0b1818f74108d
Adagradiv <arunsrivastava00765@gmail.com>
2026-01-01 21:36:51 +0530

    Add files via upload

---
+++ android/app/build/tmp/kapt3/stubs/debug/com/upgovt/ashadiary/data/api/SyncPullResponse.java
@@ -0,0 +1,82 @@
+package com.upgovt.ashadiary.data.api;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u00002\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010 \n\u0002\u0010$\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0010\t\n\u0002\b\r\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0086\b\u0018\u00002\u00020\u0001B[\u0012\u0018\u0010\u0002\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u0012\u0018\u0010\u0006\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u0012\u0018\u0010\u0007\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u0012\u0006\u0010\b\u001a\u00020\t\u00a2\u0006\u0002\u0010\nJ\u001b\u0010\u0011\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003H\u00c6\u0003J\u001b\u0010\u0012\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003H\u00c6\u0003J\u001b\u0010\u0013\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003H\u00c6\u0003J\t\u0010\u0014\u001a\u00020\tH\u00c6\u0003Jg\u0010\u0015\u001a\u00020\u00002\u001a\b\u0002\u0010\u0002\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u00032\u001a\b\u0002\u0010\u0006\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u00032\u001a\b\u0002\u0010\u0007\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u00032\b\b\u0002\u0010\b\u001a\u00020\tH\u00c6\u0001J\u0013\u0010\u0016\u001a\u00020\u00172\b\u0010\u0018\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u0019\u001a\u00020\u001aH\u00d6\u0001J\t\u0010\u001b\u001a\u00020\u0005H\u00d6\u0001R#\u0010\u0002\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000b\u0010\fR#\u0010\u0007\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\fR\u0011\u0010\b\u001a\u00020\t\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000e\u0010\u000fR#\u0010\u0006\u001a\u0014\u0012\u0010\u0012\u000e\u0012\u0004\u0012\u00020\u0005\u0012\u0004\u0012\u00020\u00010\u00040\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\f\u00a8\u0006\u001c"}, d2 = {"Lcom/upgovt/ashadiary/data/api/SyncPullResponse;", "", "beneficiaries", "", "", "", "visits", "edit_requests", "timestamp", "", "(Ljava/util/List;Ljava/util/List;Ljava/util/List;J)V", "getBeneficiaries", "()Ljava/util/List;", "getEdit_requests", "getTimestamp", "()J", "getVisits", "component1", "component2", "component3", "component4", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+public final class SyncPullResponse {
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> beneficiaries = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> visits = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> edit_requests = null;
+    private final long timestamp = 0L;
+    
+    public SyncPullResponse(@org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> beneficiaries, @org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> visits, @org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> edit_requests, long timestamp) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> getBeneficiaries() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> getVisits() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> getEdit_requests() {
+        return null;
+    }
+    
+    public final long getTimestamp() {
+        return 0L;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.List<java.util.Map<java.lang.String, java.lang.Object>> component3() {
+        return null;
+    }
+    
+    public final long component4() {
+        return 0L;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.upgovt.ashadiary.data.api.SyncPullResponse copy(@org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> beneficiaries, @org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> visits, @org.jetbrains.annotations.NotNull()
+    java.util.List<? extends java.util.Map<java.lang.String, ? extends java.lang.Object>> edit_requests, long timestamp) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public java.lang.String toString() {
+        return null;
+    }
+}
---
+++ android/app/build/tmp/kapt3/stubs/debug/com/upgovt/ashadiary/data/api/UserResponse.java
@@ -0,0 +1,96 @@
+package com.upgovt.ashadiary.data.api;
+
+@kotlin.Metadata(mv = {1, 9, 0}, k = 1, xi = 48, d1 = {"\u0000\"\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0010\b\n\u0002\b\u0010\n\u0002\u0010\u000b\n\u0002\b\u0004\b\u0086\b\u0018\u00002\u00020\u0001B-\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0003\u0012\u0006\u0010\u0005\u001a\u00020\u0003\u0012\u0006\u0010\u0006\u001a\u00020\u0007\u0012\u0006\u0010\b\u001a\u00020\u0003\u00a2\u0006\u0002\u0010\tJ\t\u0010\u0011\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0012\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0013\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0014\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\u0015\u001a\u00020\u0003H\u00c6\u0003J;\u0010\u0016\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00032\b\b\u0002\u0010\u0005\u001a\u00020\u00032\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u0003H\u00c6\u0001J\u0013\u0010\u0017\u001a\u00020\u00182\b\u0010\u0019\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010\u001a\u001a\u00020\u0007H\u00d6\u0001J\t\u0010\u001b\u001a\u00020\u0003H\u00d6\u0001R\u0011\u0010\u0005\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\n\u0010\u000bR\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\f\u0010\u000bR\u0011\u0010\u0006\u001a\u00020\u0007\u00a2\u0006\b\n\u0000\u001a\u0004\b\r\u0010\u000eR\u0011\u0010\b\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u000f\u0010\u000bR\u0011\u0010\u0004\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0010\u0010\u000b\u00a8\u0006\u001c"}, d2 = {"Lcom/upgovt/ashadiary/data/api/UserResponse;", "", "id", "", "username", "full_name", "role_id", "", "role_name", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;ILjava/lang/String;)V", "getFull_name", "()Ljava/lang/String;", "getId", "getRole_id", "()I", "getRole_name", "getUsername", "component1", "component2", "component3", "component4", "component5", "copy", "equals", "", "other", "hashCode", "toString", "app_debug"})
+public final class UserResponse {
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String id = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String username = null;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String full_name = null;
+    private final int role_id = 0;
+    @org.jetbrains.annotations.NotNull()
+    private final java.lang.String role_name = null;
+    
+    public UserResponse(@org.jetbrains.annotations.NotNull()
+    java.lang.String id, @org.jetbrains.annotations.NotNull()
+    java.lang.String username, @org.jetbrains.annotations.NotNull()
+    java.lang.String full_name, int role_id, @org.jetbrains.annotations.NotNull()
+    java.lang.String role_name) {
+        super();
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getId() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getUsername() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getFull_name() {
+        return null;
+    }
+    
+    public final int getRole_id() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getRole_name() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component1() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    public final int component4() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component5() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.upgovt.ashadiary.data.api.UserResponse copy(@org.jetbrains.annotations.NotNull()
+    java.lang.String id, @org.jetbrains.annotations.NotNull()
+    java.lang.String username, @org.jetbrains.annotations.NotNull()
+    java.lang.String full_name, int role_id, @org.jetbrains.annotations.NotNull()
+    java.lang.String role_name) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @java.lang.Override()
+    @org.jetbrains.annotations.NotNull()
+    public java.lang.String toString() {
+        return null;
+    }
+}
